7e803fc4dfc946af679667756a14861dc1d5fe7e
Jonatan Goulart <57471892+Cryover@users.noreply.github.com>
2024-05-12 20:49:21 -0300

    Update NavBar.js
    Ajusta imagem da logo

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -13,9 +13,8 @@ function NavBar() {
     <FadeInLeftNav>
       <Img
         logo="true"
-        src="/img/JL-logo.png"
+        src="https://scontent.fpoa4-1.fna.fbcdn.net/v/t39.30808-6/290318704_5359916257408612_190275830454723261_n.jpg?_nc_cat=1&ccb=1-7&_nc_sid=5f2048&_nc_ohc=_REIHEsGcAsQ7kNvgGryit7&_nc_ht=scontent.fpoa4-1.fna&oh=00_AYBIiTTGo9REeQkUFXWqt-pHwuili4z70HTNpSTF9iRMjQ&oe=66471D4C"
         alt="Logo_Jal"
-        drag
         dragConstraints={{ left: 10, right: 10, down: false, up: false }}
         dragMomentum={false}
       ></Img>
